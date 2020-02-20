9ab4c597cdccee659cbc291ecf9b459c3e9dd488
Bogdan Berdnikov <bogdan_krg@rocketfirm.com>
2020-02-20 13:53:31 +0600

    feat: setup typescript

---
+++ src/boot.js
@@ -0,0 +1,4 @@
+function speak(message) {
+    console.log(message);
+}
+speak('Ready fox! I\'m listening');
---
+++ src/boot.ts
@@ -0,0 +1,4 @@
+function speak(message: string) {
+  console.log(message);
+}
+speak('Ready fox! I\'m listening');
