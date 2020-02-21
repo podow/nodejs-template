31934f1f121b5860d86fad58452ad3cd8e1ca9d8
Bogdan Berdnikov <bogdan_krg@rocketfirm.com>
2020-02-21 13:34:36 +0600

    feat: add database connection

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,12 @@
 import express from 'express';
-import redis from './utils/redisClient';
-import sequilize from './utils/sequilizeClient'
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.get('/', async (req: any, res: any) => {
-  sequilize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    })
-    .catch((err: string) => {
-      console.error('Unable to connect to the database:', err);
-    });
-  // const {key} = req.query;
-
-  // if (key) {
-  //   await redis.setAsync('key', key);
-  // }
-  
-  // if (redis.getAsync('key')) {
-  //   const data = await redis.getAsync('key');
-  //   res.send(data);
-  // }
-
   res.send('Hello World!');
 });
-  
-app.listen(8080, () => {
+
+app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
 });
--- src/utils/sequilizeClient.ts
+++ src/utils/sequilizeClient.ts
@@ -1,6 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-export default new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+const {
+    DATABASE_HOST,
+    DATABASE_NAME,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+} = process.env;
+
+// @ts-ignore
+export default new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
+    host: DATABASE_HOST,
+    dialect: 'postgres', /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 });
